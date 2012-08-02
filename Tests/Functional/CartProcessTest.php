5f99bd06df8f30a9109f9e7954baf85bcd51bf74
iampersistent <develop@zestic.com>
2012-08-01 23:51:13 -0700

    a little more test refactoring

--- Tests/Functional/CartProcessTest.php
+++ Tests/Functional/CartProcessTest.php
@@ -13,53 +13,52 @@ class CartProcessTest extends CartTestCommon
 
     public function testProcessCart()
     {
-        $cartManager = $this->getKernel()->getContainer()->get('vespolina.cart_manager');
+        $this->markTestIncomplete('the pricing needs to be tested before this test can be useful');
 
         $customerId = '1248934893';
 
-        $product1 = $this->createProduct('Ipad 2', 'IPAD-2011', 499);
+        $product1 = $this->createProduct('Ipad 2', 499);
+        $product2 = $this->createProduct('Iphone 4S');
+        $this->dm->persist($product1);
+        $this->dm->persist($product2);
+        $this->dm->flush();
 
-        $product2 = new Cartable();
-        $product2->setName('Iphone 4S');
-        $product2->setId('IPHONE-4S-2011');
-
-        $cart = $cartManager->createCart();
+        $cart = $this->cartMgr->createCart();
 
         $cart->setOwner($customerId);
         $cart->setExpiresAt(new \DateTime('now + 2 days'));
 
-        $cartItem1 = $cartManager->addItemToCart($cart, $product1);
-        $cartManager->setItemQuantity($cartItem1, 10);
+        $cartItem1 = $this->cartMgr->addProductToCart($cart, $product1);
+        $this->cartMgr->setItemQuantity($cartItem1, 10);
         $cartItem1->getPricingSet()->set('unitPrice', 499);
 
         $cartItem1->addOption('color', 'white');
         $cartItem1->addOption('connectivity', 'WIFI+3G');
         $cartItem1->addOption('size', '64GB');
 
-        $cartManager->setItemQuantity($cartItem1, 3);
-        $cartManager->setCartItemState($cartItem1, 'init');
+        $this->cartMgr->setItemQuantity($cartItem1, 3);
+        $this->cartMgr->setCartItemState($cartItem1, 'init');
 
         $this->assertEquals($cartItem1->getName(), $product1->getName());
 
-        $cartItem2 = $cartManager->addItemToCart($cart, $product2);
-        $cartManager->setItemQuantity($cartItem2, 2);
+        $cartItem2 = $this->cartMgr->addItemToCart($cart, $product2);
+        $this->cartMgr->setItemQuantity($cartItem2, 2);
 
         $cartItem2->getPricingSet()->set('unitPrice', 699);
-        $cartManager->setCartItemState($cartItem2, 'init');
+        $this->cartMgr->setCartItemState($cartItem2, 'init');
 
         $testCartItem1 = $cart->getItem(1);
 
         $cartOwner = $cart->getOwner();
         $this->assertEquals($cartOwner, $customerId);
 
-
         //Calculate prices
-        $cartManager->determinePrices($cart);
+        $this->cartMgr->determinePrices($cart);
 
-        $cartManager->updateCart($cart);
+        $this->cartMgr->updateCart($cart);
 
         //Step two, find back the open cart
-        $aCart = $cartManager->findOpenCartByOwner($customerId);
+        $aCart = $this->cartMgr->findOpenCartByOwner($customerId);
         $this->assertEquals(count($aCart->getItems()), 2);
 
         $aCartItem1 = $aCart->getItem(1);
@@ -69,9 +68,9 @@ class CartProcessTest extends CartTestCommon
 
         //...and close it
         $aCart->setFollowUp('sales_order_12093488');
-        $cartManager->setCartState($aCart, Cart::STATE_CLOSED);
+        $this->cartMgr->setCartState($aCart, Cart::STATE_CLOSED);
 
-        $cartManager->updateCart($aCart, true);
+        $this->cartMgr->updateCart($aCart, true);
 
         $aCart->clearItems();
         $this->assertEquals($aCart->getItems()->count(), 0);
